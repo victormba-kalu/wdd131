6396c1ec87325b0a70132231fd7f74431431668a
victormba-kalu <mbavictorkalu@gmail.com>
2025-05-19 20:00:08 +0100

    debugged the code

--- week02/scripts.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector("#favchapa");
-const button = document.querySelector("button");
-const list = document.querySelector("ul");
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
-li.textContent = input.value;
-deleteButton.textContent = "❌";
-li.append(deleteButton);
-list.append(li);
